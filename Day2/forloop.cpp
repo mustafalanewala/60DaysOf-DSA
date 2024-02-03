43824f939724b07dd6562b12826c25a89cc607c0
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-02-03 14:43:48 +0530

    Create forloop.cpp

---
+++ Day2/forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{    
+    int n;
+    cin >> n;
+    
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout << "Sum of total is " << sum << endl;
+    return 0;
+}
