2bce99a08d933edd6bfed5e59cfcb3fe421f2eae
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-02-03 16:54:24 +0530

    Update Day1

---
+++ Day1/evenodd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if(n%2==0){
+        cout << "Num is even" << endl;
+    } else {
+        cout << "Num is odd" << endl;
+    }
+    return 0;
+}
